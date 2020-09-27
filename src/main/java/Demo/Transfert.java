03a477b43755d884a7231d273d2ee5655dd9245d
Mohamed Koita <Mohamed@Mohameds-MacBook-Pro.local>
2020-09-27 14:36:10 -0400

    new

--- src/main/java/Demo/Transfert.java
+++ src/main/java/Demo/Transfert.java
@@ -1,5 +1,24 @@
 package Demo;
 
 public class Transfert {
+    private int amount;
+    private String sender;
+    private String receiver;
+    private String password;
 
+    public Transfert(int amount, String sender, String receiver, String password) {
+        this.amount = amount;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Transfert{" +
+                "amount=" + amount +
+                ", sender='" + sender + '\'' +
+                ", receiver='" + receiver + '\'' +
+                '}';
+    }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import Demo.Transfert;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Transfert trans = new Transfert(10000, "Maman", "Mohamed", "Maman1234");
+
+        System.out.print(trans);
+
+    }
+}
